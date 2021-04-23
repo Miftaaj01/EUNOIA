7a0e531c1dc66c408292d549b741aeec857ed322
Vallentina Wahyu F <79146280+vallentinawf@users.noreply.github.com>
2021-04-23 14:04:24 +0800

    Add files via upload

---
+++ QuestionLibrary/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuestionLibrary
+{
+    public class Question
+    {
+        public string SadCategory;
+        public string HappyCategory;
+        public string StressCategory;
+        public string OthersCategory;
+
+        public static void ShowQuestion()
+        {
+
+        }
+    }
+}
