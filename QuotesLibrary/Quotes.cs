d1d0d54c92c4ee73091816f403f61b9eec8a82c3
Vallentina Wahyu F <79146280+vallentinawf@users.noreply.github.com>
2021-04-23 14:02:32 +0800

    Add files via upload

---
+++ QuotesLibrary/Quotes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QuotesLibrary
+{
+    public class Quotes
+    {
+        public string wisdomQuotes;
+
+        public static void displayQuotes()
+        {
+
+        }
+    }
+}
